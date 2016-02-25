8a0c51bef7e5fa9aac9cef6553f17354f8cac725
Brad Anderson <brad.anderson87@gmail.com>
2016-02-24 19:41:55 -0600

    Allows instances to be json files. Changes how the `partial` "macro" is defined.

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 'use strict';
 
 const nunjucks = require('nunjucks');
+const fs = require('fs');
 
 
-const res = nunjucks.render('tests/bio/_bio.html');
+const res = nunjucks.render('tests/bio/_bio.html', {
+    partial: function (path, model) {
+        return new nunjucks.runtime.SafeString(nunjucks.render(path, { model: model }));
+    },
+    json: function (path) {
+        return JSON.parse(fs.readFileSync(path, { encoding: 'utf-8' }));
+    }
+});
 console.log(res);
